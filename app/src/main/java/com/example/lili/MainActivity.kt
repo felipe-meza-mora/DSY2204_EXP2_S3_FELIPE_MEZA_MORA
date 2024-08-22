659743962c5095364af03b47c7cd7d485c034638
felipe-meza-mora <feli.meza@duocuc.cl>
2024-08-21 23:07:44 -0400

    Primer Committ

---
+++ app/src/main/java/com/example/lili/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.lili
+
+import android.os.Bundle
+
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.ui.Modifier
+import com.example.lili.screens.SplashScreen
+import com.example.lili.ui.theme.LiliTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            LiliTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                    ) {
+                    Text(text = "Estamos en Home")
+                }
+            }
+        }
+    }
+}
